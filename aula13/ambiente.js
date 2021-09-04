7a8bdde295a9c69327a3bee27a5bafa4050cfb3a
valeriasouza28 <valleriasouza28@gmail.com>
2021-09-03 22:12:17 -0300

    add aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+var c = 1;
+do {
+    console.log(`Passo ${c} `)
+    c++
+} while (c <= 6)
+
+
+
+
+/*variável c, que é a váriável de contador, que vai começar com 1, é a primeira vez escrita*/
+/*var c = 1;*/
+/*enquanto o contador for menor do que 6, */
+/*while (c <= 500) {
+console.log(`Passo ${c}`)*/
+/* esse c++ significa que c recebe c mais 1*/
+/*c++
+}
+*/
+
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
