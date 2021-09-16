d6cce6f34f1048fa597c1f31bac40da8dd0845d1
valeriasouza28 <valleriasouza28@gmail.com>
2021-09-16 14:15:32 -0300

    add aula24

---
+++ aula14/ambiente.js
@@ -0,0 +1,8 @@
+
+
+console.log('Vai Começar')
+for ( var c = 1 ;  c <= 4; c++) {
+  console.log(c)
+}
+console.log('FIM')
+/*Esse código vai escrever Vai Começar, vai contar até 4, e vai escrever FIM*/
